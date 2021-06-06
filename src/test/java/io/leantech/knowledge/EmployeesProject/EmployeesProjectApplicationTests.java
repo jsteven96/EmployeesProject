6a3c0b5e30de5caf6853d0b0866a08c4c64b3326
Javier Steven Pérez Fernández <perez1932@hotmail.es>
2021-06-05 22:12:52 -0500

    Commit inicial del proyecto

---
+++ src/test/java/io/leantech/knowledge/EmployeesProject/EmployeesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.leantech.knowledge.EmployeesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
